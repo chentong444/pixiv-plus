064aeea19483c37d4c8839187bf424af353fd3d5
chentong444 <chentong444@126.com>
2020-09-06 14:36:23 +0800

    verson 2.1

--- background.js
+++ background.js
@@ -1,48 +1,89 @@
 "use strict";
 
-function on_click(info, tab){
-	var url='https://iqdb.org/?url='+encodeURIComponent(info.srcUrl);
-	chrome.tabs.create({url});
+function on_click(info, tab) {
+    var url = 'https://iqdb.org/?url=' + encodeURIComponent(info.srcUrl);
+    chrome.tabs.create({
+        url
+    });
 }
 
 chrome.contextMenus.create({
-	type:'normal',
-	//id:'download_image',
-	title:'search by iqdb',
-	contexts:['image'],
-	onclick:on_click
+    type: 'normal',
+    //id:'download_image',
+    title: 'search by iqdb',
+    contexts: ['image'],
+    onclick: on_click
 });
 
-var setHookForReferer = function() {
-    var filter = {
-        urls: ["*://*.pixiv.net/*", "*://*.pximg.net/*"],
-        types: ["other"]
-    };
-    var options = ["blocking", "requestHeaders"];
-    chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
-        if (details.url.match(/img\-original/)) {
-            details.requestHeaders.push({
-                name: "Referer",
-                value: "http://www.pixiv.net/member_illust.php"
-            });
-        }
-        return {requestHeaders: details.requestHeaders};
-    }, filter, options);
-};
-setHookForReferer();
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  var type=request.type;
-  switch (type){
-    case "download_img":
+var map = new Map();
 
-      chrome.downloads.download({
-        url:request.url,
-        
-        conflictAction: "prompt"
-      });
-      break;
-    default:
-      break;
-  }
+chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
+suggest({filename: map.get(item.id),
+conflictAction: "prompt"});
+map.delete(item.id);
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    var type = request.type;
+    switch (type) {
+        case "download_img":
+            var a = document.createElement('a');
+            a.href = request.url;
+
+            if (request.forceDownload || a.protocol === 'blob:') {
+                // No workaround needed or the XHR workaround is giving
+                // us its results.
+                chrome.downloads.download({
+                    url: request.url,
+                    filename: request.filename,
+                    conflictAction: "prompt"
+                },function(downloadId) {
+                    map.set(downloadId,request.filename);
+                });
+
+                if (a.protocol === 'blob:') {
+                    // We don't need the blob URL any more. Release it.
+                    URL.revokeObjectURL(request.url);
+                }
+            } else {
+                // The XHR workaround is needed. Inform the tab.
+                chrome.tabs.sendMessage(sender.tab.id, {
+                    action: 'xhr-download',
+                    url: request.url
+                });
+            }
+            break;
+        case "test_marked":
+            fetch('https://www.pixiv.net/artworks/' + request.id).then(function(response) {
+                response.text().then(function(text) {
+                    var bookmark = text.match(request.id +
+                        "\":(.*)\"bookmarkData\":(.*),");
+                    if (bookmark[2].substring(0, 4) != 'null') {
+                        chrome.tabs.sendMessage(sender.tab.id, {
+                            action: 'xhr-marked' + request.id
+                        });
+                    }
+
+                });
+            }).catch(error => console.error('Error:', error));
+
+            break;
+        case "test_followed":
+            fetch('https://www.pixiv.net/member.php\\?id=' + request.id).then(function(response) {
+                response.text().then(function(text) {
+                    var isFollowed = text.match("\"isFollowed\":(.*),\"");
+                    if (isFollowed[1].substring(0, 4) == 'true') {
+                        chrome.tabs.sendMessage(sender.tab.id, {
+                            action: 'xhr-followed' + request.id
+                        });
+                    }
+
+                });
+            }).catch(error => console.error('Error:', error));
+
+            break;
+        default:
+            break;
+    }
 
 });
--- myjs.js
+++ myjs.js
@@ -1,28 +1,15 @@
-
 'use strict';
 
 function SetCSS() {
-    var css_rule_text = 'Button {padding:0; margin:0;width:100px; height:100px; background-color:#006aff; filter: opacity(40%)}',
-    pa = document.querySelector('head'),
-    css_style = document.createElement('style'),
-    css_rule = document.createTextNode(css_rule_text);
+    var css_rule_text =
+        'Button {padding:0; margin:0;width:100px; height:100px;filter: opacity(20%)}',
+        pa = document.querySelector('head'),
+        css_style = document.createElement('style'),
+        css_rule = document.createTextNode(css_rule_text);
     css_style.type = 'text/css';
     css_style.className = 'mycss-rule';
     css_style.appendChild(css_rule);
     pa.appendChild(css_style);
-}
-
-function get_img_src(option) {
-    if (option === 'manga_page') {
-        var img = document.getElementsByTagName("img")[0];
-        return img.src;
-    } else if (option === 'medium') {
-        var img = document.getElementsByClassName("_1V2heP9")[0];
-        var imgson = img.getElementsByTagName("img")[0];
-        return imgson.src;
-    } else {
-        return null;
-    }
 }
 
 function ChangeSize(img, is_first) {
@@ -32,50 +19,61 @@ function ChangeSize(img, is_first) {
         'fit-width': 'full'
     };
     var saved_image_size = localStorage['pixiv_plus_saved_image_size'];
-    var image_size = (image_size_types[saved_image_size]) ? (image_size_types[saved_image_size]) : 'full';
+    var image_size = (image_size_types[saved_image_size]) ? (image_size_types[saved_image_size]) :
+        'full';
     image_size = is_first ? saved_image_size : image_size;
     switch (image_size) {
-    case 'full':
-        img.style.maxHeight = "none";
-        img.style.maxWidth = "none";
-        break;
-    case 'fit-height':
-        img.style.maxHeight = window.innerHeight + "px";
-        img.style.maxWidth = "none";
-        break;
-    case 'fit-width':
-        img.style.maxHeight = "none";
-        img.style.maxWidth = window.innerWidth + "px";
-        break;
+        case 'full':
+            img.style.maxHeight = "none";
+            img.style.maxWidth = "none";
+            break;
+        case 'fit-height':
+            img.style.maxHeight = window.innerHeight + "px";
+            img.style.maxWidth = "none";
+            break;
+        case 'fit-width':
+            img.style.maxHeight = "none";
+            img.style.maxWidth = window.innerWidth + "px";
+            break;
     }
     localStorage['pixiv_plus_saved_image_size'] = image_size;
 }
 
 // function get_img_filename(img_url){
-// 	var img_filename=
+//  var img_filename=
 // }
 
-function SetButton(button_position, option) {
+function SetButton(button_position, color) {
     var buttonContainer = document.createElement("div");
     buttonContainer.style.textAlign = 'center';
+    buttonContainer.style.zIndex = 100;
+    buttonContainer.style.width = '300px';
+    buttonContainer.style.position = 'relative';
+    //buttonContainer.style.top = document.body.clientHeight + 'px';
+    buttonContainer.style.left = (document.body.clientWidth / 2 - 150).toString() + 'px';
 
     var download_button = document.createElement("button");
     var close_button = document.createElement("button");
 
-    var download_url = get_img_src(option);
+    download_button.style.backgroundColor = color;
+    close_button.style.backgroundColor = color
     //var download_filename=get_img_filename(download_url);
 
     download_button.className = 'download_btn';
-    download_button.onmouseover = function () {
-        download_button.style.filter = "opacity(100%)";
+    download_button.onmouseover = function() {
+        download_button.style.filter = "opacity(80%)";
     };
-    download_button.onmouseout = function () {
-        download_button.style.filter = "opacity(40%)";
+    download_button.onmouseout = function() {
+        download_button.style.filter = "opacity(20%)";
     };
-    download_button.addEventListener("click", function () {
+    download_button.addEventListener("click", function() {
+        var img = button_position.getElementsByTagName("img")[0];
+        var download_url = img.src;
         chrome.runtime.sendMessage({
             type: "download_img",
-            url: download_url
+            url: download_url,
+            forceDownload: false
+
             //filename:download_filename;
         });
         event.stopPropagation();
@@ -85,25 +83,28 @@ function SetButton(button_position, option) {
     var download_bgImage = document.createElement("img");
     var download_bgimg = chrome.extension.getURL("images/download.png");
     download_bgImage.src = download_bgimg;
+    download_bgImage.draggable = false;
     download_bgImage.style.height = '90px';
     download_button.appendChild(download_bgImage);
 
     buttonContainer.appendChild(download_button);
 
     close_button.className = 'close_btn';
-    close_button.onmouseover = function () {
-        close_button.style.filter = "opacity(100%)";
+    close_button.onmouseover = function() {
+        close_button.style.filter = "opacity(80%)";
     };
-    close_button.onmouseout = function () {
-        close_button.style.filter = "opacity(40%)";
+    close_button.onmouseout = function() {
+        close_button.style.filter = "opacity(20%)";
     };
-    close_button.addEventListener("click", function () {
-        (window.open('', '_self')).close();
+    close_button.addEventListener("click", function() {
+        //(window.open('', '_self')).close();
+        open(location, '_self').close();
     });
 
     var close_bgImage = document.createElement("img");
     var close_bgimg = chrome.extension.getURL("images/close.png");
     close_bgImage.src = close_bgimg;
+    close_bgImage.draggable = false;
     close_bgImage.style.height = '90px';
     close_button.appendChild(close_bgImage);
 
@@ -115,41 +116,146 @@ function SetButton(button_position, option) {
 
 function AddEvent(event, img) {
     switch (event.keyCode) {
-    case 81:
-        ChangeSize(img, 0);
-        break;
+        case 81:
+            ChangeSize(img, 0);
+            break;
 
     }
 }
+
+function colorMaked(id, worksbody) {
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        switch (message.action) {
+            case 'xhr-marked' + id:
+                worksbody.style.backgroundColor = "#FFC0CB";
+                break;
+        }
+    });
+    chrome.runtime.sendMessage({
+        type: "test_marked",
+        id: id,
+    });
+}
+
+function colorFollowed(id, body) {
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        switch (message.action) {
+            case 'xhr-followed' + id:
+                body.style.backgroundColor = "#87CEFA";
+                break;
+        }
+    });
+    chrome.runtime.sendMessage({
+        type: "test_followed",
+        id: id,
+    });
+}
+
 function initialize() {
     SetCSS();
-    if (window.location.href.match('manga_big')) {
+    if (window.location.href.match('artworks')) {
+        chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+            switch (message.action) {
+                case 'xhr-download':
+                    var xhr = new XMLHttpRequest();
+                    xhr.responseType = 'blob';
 
-        var img = document.getElementsByTagName("img")[0];
-        ChangeSize(img, 1);
-        SetButton(document.body, 'manga_page');
-        document.onkeydown = function (event) {
-            AddEvent(event, img);
-        };
-    } else if (window.location.href.match('medium')) {
-        var obser = new MutationObserver(function (records) {
-                var button_position = document.getElementsByClassName("_1V2heP9")[0];
-                if (typeof(button_position) != 'undefined') {
-                    var img = document.getElementsByClassName("_1V2heP9")[0];
-                    var imgson = img.getElementsByTagName("img")[0];
+                    xhr.addEventListener('load', (e) => {
+                        chrome.runtime.sendMessage({
+                            type: "download_img",
+                            url: URL.createObjectURL(xhr.response),
+                            filename: message.url.substr(message.url.lastIndexOf(
+                                '/') + 1),
+                            forceDownload: true
+                        });
+                    });
+
+                    xhr.addEventListener('error', (e) => {
+                        // The XHR method failed. Force an attempt using the
+                        // downloads API.
+                        chrome.runtime.sendMessage({
+                            type: "download_img",
+                            url: message.url,
+                            forceDownload: true
+                        });
+                    });
+
+                    xhr.open('get', message.url, true);
+                    xhr.send();
+                    break;
+            }
+        });
+        var obser = new MutationObserver(function(records) {
+            var role_list = document.querySelectorAll("div[role]");
+            var role = role_list[role_list.length - 1];
+            var color;
+            if (typeof(role) == 'undefined')
+                return;
+            if (role.firstChild != null) {
+                var button_position = role.firstChild.firstChild;
+                if (button_position != null && typeof(button_position) != 'undefined') {
+                    if (role.firstChild.childNodes.length == 1) {
+                        color = "#006aff";
+                    } else {
+                        color = "#32ea49";
+                    }
+                    var imgson = button_position.getElementsByTagName("img")[0];
+                    if (typeof(imgson) == 'undefined')
+                        return;
                     ChangeSize(imgson, 1);
-                    SetButton(button_position, 'medium')
-                    document.onkeydown = function (event) {
+                    SetButton(button_position, color);
+                    document.onkeydown = function(event) {
                         AddEvent(event, imgson);
                     };
                 }
-            });
+            }
+        });
         obser.observe(document.documentElement, {
             childList: false,
             subtree: false,
             attributes: true
         });
+    } else if (window.location.href.match('www.pixivision.net\/zh\/a\/')) {
+        var am_works = document.getElementsByClassName("am__body");
+        if (am_works.length != 1) {
+            return;
+        }
+        var works = am_works[0].children;
+        for (var i = 0; i < works.length - 1; i++) {
+            var linkNode = works[i].getElementsByClassName('inner-link')[1];
+            var id = linkNode.href.match("[0-9]+(?=[^0-9]*$)");
+            colorMaked(id[0], works[i]);
+        }
+    } else if (window.location.href.match('ranking.php\\?mode=daily')) {
+        var num = 0;
+        var ranking_items = document.getElementsByClassName("ranking-items adjust");
+        if (ranking_items.length != 1) {
+            return;
+        }
+        var obser = new MutationObserver(function(records) {
+
+            var items = ranking_items[0].children;
+            for (var i = num; i < items.length; i++) {
+                var linkNode = items[i].getElementsByClassName('work _work')[0];
+                var id = linkNode.href.match("[0-9]+(?=[^0-9]*$)");
+                colorMaked(id[0], linkNode.parentNode);
+
+                var author=items[i].getElementsByClassName('user-container ui-profile-popup')[0];
+                var author_id = author.href.match("[0-9]+(?=[^0-9]*)");
+                colorFollowed(author_id[0],author);
+            }
+            num = items.length;
+        });
+        obser.observe(ranking_items[0], {
+            childList: true,
+            subtree: false
+    
+        });
+
+
+
     }
 }
 
 initialize();
+
